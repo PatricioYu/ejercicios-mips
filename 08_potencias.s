7c8ac7ec34377e9115b2bb8b5648691ca0b951e7
Yu Patricio <yupatricio0@gmail.com>
2021-08-21 23:00:33 -0300

    auto aclaraciones de la consigna 8

--- 08_potencias.s
+++ 08_potencias.s
@@ -1,6 +1,7 @@
 # Escribir un programa que imprima las primeras n potencias de dos, siendo n un número ingresado por el usuario. Al finalizar el programa debe imprimir la suma de las potencias calculadas.
+# En vez de hacer solo potencia de base 2, tomo la base que ingrese el usuario y la potencio la cantidad de veces que desee el usuario.
 .data
-n:        .asciiz "Ingrese un número: \n"
+base:        .asciiz "Ingrese un número: \n"
 potencia: .asciiz "Ingrese las veces que desea elevarlo: \n"
 msg:      .asciiz "\nEl resultado es: \n"
 suma:     .asciiz "\nLa suma de todas las potencias es: \n"
@@ -8,52 +9,51 @@ suma:     .asciiz "\nLa suma de todas las potencias es: \n"
 .text
 .globl main
 main:
-  li      $s0, 1
+  li      $s0, 1                # inicializo contador
 
   li      $v0, 4
-  la      $a0, n
-  syscall
-  li      $v0, 5
-  syscall
-  move    $t0 ,$v0 
+  la      $a0, base
+  syscall                       # leo mensaje de base
+  li      $v0, 5                
+  syscall                       # guardo el valor ingresado por el usuario
+  move    $t0 ,$v0              # muevo ese valor desde $v0 a $t0
 
   li      $v0, 4
   la      $a0, potencia
-  syscall
+  syscall                       # leo mensaje de potencia
   li      $v0, 5
-  syscall
-  move    $t1, $v0
+  syscall                       # guardo el valor ingresado por el usuario
+  move    $t1, $v0              # muevo ese valor desde $v0 a $t1
 
-  li      $t2 0
+  li      $t2, 0                # inicializo acumulador
   add     $t2, $t2, $t0
 
-  li      $t3, 0
+  li      $t3, 0                # inicializo suma
   add     $t3, $t3, $t0
 
 
 loop:
   beq     $s0, $t1, exit
-  addi    $s0, $s0, 1
-  mul     $t2, $t2, $t0
-  add     $t3, $t3, $t2
+  addi    $s0, $s0, 1           # contador
+  mul     $t2, $t2, $t0         # acumulador
+  add     $t3, $t3, $t2         # suma
   j       loop
 
 
 exit:
   li      $v0, 4
   la      $a0, msg
-  syscall
+  syscall                       # printeo msg
   li      $v0, 1
   move    $a0, $t2
-  syscall
+  syscall                       # printeo el resultado de la potencia
 
   li      $v0, 4
   la      $a0, suma
-  syscall
+  syscall                       # printeo el mensaje de suma
   li      $v0, 1
   move    $a0, $t3
-  syscall
+  syscall                       # printeo la suma de las potencias
 
   li      $v0, 10
-  syscall
-
+  syscall                       # cierro mi programa
